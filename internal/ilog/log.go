58e22ad968ec8291943a8e044e4f09c1aed2330a
starudream <justwangsheng@gmail.com>
2022-11-30 17:44:17 +0800

    perf: logger use INFO level

--- internal/ilog/log.go
+++ internal/ilog/log.go
@@ -8,7 +8,7 @@ import (
 )
 
 var X = func() zerolog.Logger {
-	lc := zerolog.New(&zerolog.ConsoleWriter{Out: colorable.NewColorableStdout(), TimeFormat: constant.LoggerTimeFormat}).With()
+	lc := zerolog.New(&zerolog.ConsoleWriter{Out: colorable.NewColorableStdout(), TimeFormat: constant.LoggerTimeFormat}).Level(zerolog.InfoLevel).With()
 	if constant.VERSION != "" {
 		lc = lc.Str("version", constant.VERSION)
 	}
